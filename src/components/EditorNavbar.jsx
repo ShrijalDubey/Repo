fa71c04d5dd48fc2d680858d62c7039b2c800108
ShrijaDubey <shrijaldubey36@gmail.com>
2025-11-01 21:33:40 +0530

    update version

--- src/components/EditorNavbar.jsx
+++ src/components/EditorNavbar.jsx
@@ -22,8 +22,6 @@ export default function EditorNavbar()  {
         </div>
     </div>
     <CodeEditor />
-
-
     </>     
   );
 }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,11 +5,11 @@ export default function Navbar() {
         <div className="box">
             <h2><a className="logo" href="/codepractice">REPO</a></h2>
             <ul>
-                <li><a class="btn" href="/editor"><i class="fa-solid fa-code"></i>     Editor</a></li>
-                <li><a class="btn" href="/community"><i class="fa-solid fa-user-group"></i>     Community</a></li>
-                <li><a class="btn" href="/doubts"><i class="fa-regular fa-message"></i>     Doubts</a></li>
-                <li><a class="btn" href="/profile"><i class="fa-regular fa-user"></i>     Profile</a></li>
-                <li><a class="login-btn btn" href="/login"><i class="fa-solid fa-right-to-bracket"></i>     Login</a></li>
+                <li><a className="btn" href="/editor"><i className="fa-solid fa-code"></i>     Editor</a></li>
+                <li><a className="btn" href="/community"><i className="fa-solid fa-user-group"></i>     Community</a></li>
+                <li><a className="btn" href="/doubts"><i className="fa-regular fa-message"></i>     Doubts</a></li>
+                <li><a className="btn" href="/profile"><i className="fa-regular fa-user"></i>     Profile</a></li>
+                <li><a className="login-btn btn" href="/login"><i className="fa-solid fa-right-to-bracket"></i>     Login</a></li>
             </ul>
         </div>
     </nav>
