7da627ad29f4b812040b66558f462df4e7816851
ShrijaDubey <shrijaldubey36@gmail.com>
2025-11-01 21:49:45 +0530

    links

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,29 @@
 import React from "react";
-import EditorNavbar from "./components/EditorNavbar";
-import Footer from "./components/footerbox";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar";
-import Cards from "./components/cards";
 import Body from "./components/body";
-
+import Cards from "./components/cards";
+import Footer from "./components/footerbox";
+import EditorNavbar from "./components/EditorNavbar";
 
 export default function App() {
   return (
     <>
-    <Navbar />
-    <Body />
-    <Cards />
-    <Footer />
-    <EditorNavbar />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={
+          <>
+            <Body />
+            <Cards />
+            <Footer />
+          </>
+        } />
+        <Route path="/editor" element={<EditorNavbar />} />
+        <Route path="/community" element={<h1 style={{color:'white'}}>Community Page</h1>} />
+        <Route path="/doubts" element={<h1 style={{color:'white'}}>Doubts Page</h1>} />
+        <Route path="/profile" element={<h1 style={{color:'white'}}>Profile Page</h1>} />
+        <Route path="/login" element={<h1 style={{color:'white'}}>Login Page</h1>} />
+      </Routes>
     </>
   );
 }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,17 +1,19 @@
-import './Navbar.css';
+import './navbar.css';
+import { Link } from 'react-router-dom';
+
 export default function Navbar() {
   return (
     <nav className="container">
-        <div className="box">
-            <h2><a className="logo" href="/codepractice">REPO</a></h2>
-            <ul>
-                <li><a className="btn" href="/editor"><i className="fa-solid fa-code"></i>     Editor</a></li>
-                <li><a className="btn" href="/community"><i className="fa-solid fa-user-group"></i>     Community</a></li>
-                <li><a className="btn" href="/doubts"><i className="fa-regular fa-message"></i>     Doubts</a></li>
-                <li><a className="btn" href="/profile"><i className="fa-regular fa-user"></i>     Profile</a></li>
-                <li><a className="login-btn btn" href="/login"><i className="fa-solid fa-right-to-bracket"></i>     Login</a></li>
-            </ul>
-        </div>
+      <div className="box">
+        <p><Link className="logo" to="/">REPO</Link></p>
+        <ul>
+          <li><Link className="btn" to="/editor"><i className="fa-solid fa-code"></i> Editor</Link></li>
+          <li><Link className="btn" to="/community"><i className="fa-solid fa-user-group"></i> Community</Link></li>
+          <li><Link className="btn" to="/doubts"><i className="fa-regular fa-message"></i> Doubts</Link></li>
+          <li><Link className="btn" to="/profile"><i className="fa-regular fa-user"></i> Profile</Link></li>
+          <li><Link className="login-btn btn" to="/login"><i className="fa-solid fa-right-to-bracket"></i> Login</Link></li>
+        </ul>
+      </div>
     </nav>
   );
 }
